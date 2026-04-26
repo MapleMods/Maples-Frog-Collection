cce0759daff4377afaa1987270a536129507e732
MapleMods <250133667+MapleMods@users.noreply.github.com>
2026-04-26 11:59:05 +0200

    Maple's Frog Collection 1.0 for 26.1.2

---
+++ README.md
@@ -0,0 +1,9 @@
+# Maple's Frog Collection
+
+Minecraft 26.1.2 | Version 1.0
+
+Download the mod here! :)
+
+CurseForge: https://legacy.curseforge.com/minecraft/mc-mods/maples-frog-collection
+
+Modrinth: https://modrinth.com/mod/maples-frog-collection
---
+++ common/src/main/java/io/github/maplemods/maplesfrogcollection/data/Constants.java
@@ -0,0 +1,11 @@
+package io.github.maplemods.maplesfrogcollection.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "maplesfrogcollection";
+	public static final String MOD_NAME = "Maple's Frog Collection";
+	public static final String MOD_VERSION = "1.0";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
